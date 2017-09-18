260523dab51667fb3f8ca0ec89c7ca2280a0c4c8
Jing25 <darkljdyx@126.com>
2017-09-18 14:52:50 -0700

    add some functions

--- TobiiTrim.R
+++ TobiiTrim.R
@@ -31,12 +31,12 @@ TobiiTrim<-function(recs, fileroot, n.aoi){
     
     
     # delete missing data
-   # for (j in 1:length(dat[,1])){
-    #  if(is.na(dat[j, xCol]) == TRUE)
+    #for (j in 1:length(dat[,1])){
+     # if(is.na(dat[j, 1]) == TRUE)
      # {
-     #   dat <- dat[-j,]
+      #  print(j)
     #  }
-    #}
+   # }
     
     ### This is the main bit
     
@@ -73,7 +73,7 @@ TobiiTrim<-function(recs, fileroot, n.aoi){
       if(is.na(aoi.tag[j,])==FALSE)    {aoi.tag[j,] <- substr(aoi.tag[j,], start = 5, stop = (nchar(aoi.tag[j,])-4))}
     }
     
-    #aoi.tag[is.na(aoi.tag)]<-"NA"
+    aoi.tag[is.na(aoi.tag)]<-"NA"
     
     ## This extracts all x axis movement       
     
@@ -106,7 +106,201 @@ TobiiTrim<-function(recs, fileroot, n.aoi){
   #########  This is where the main loop ends, below is tidying up
   
   dat<-dat3
-  #dat<-dat[is.na(dat$tag)==F,]
+  dat<-dat[is.na(dat$ParticipantName)==F,]
   
   return(dat)
+}
+
+## This function extracts the sum fixation duration
+
+SumFixDur <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName) , aois = unique(trimmed.data$aoi.tag[trimmed.data$aoi.tag != "NA"])){
+  
+  if(missing(trimmed.data)) {stop("You need to enter a data frame")}
+  
+  sum.fix.dur.out<-as.data.frame(matrix(nrow=length(recs), ncol=1, NA))
+  names(sum.fix.dur.out)<-"Sum of fixations"
+  row.names(sum.fix.dur.out)<-(recs)
+  
+  
+  
+  
+  for (i in 1:length(sum.fix.dur.out[,1])){
+    sum.fix.dur.out[i,1]<-sum(trimmed.data$GazeEventDuration[
+      (trimmed.data$ParticipantName == paste(recs[i])) &
+        sapply(trimmed.data$aoi.tag, function(check){any(check==aois)})
+      ], na.rm=TRUE)
+  }
+  return(sum.fix.dur.out)
+}
+
+## This function extracts the number of fixations
+NumFixDur <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName)){
+  
+  if(missing(trimmed.data)) {stop("You need to enter a data frame")}
+  
+  num.fix.dur <- as.data.frame(matrix(nrow=length(recs), ncol=1, NA))
+  names(num.fix.dur) <- "Number of fixations"
+  row.names(num.fix.dur) <- (recs)
+  
+  
+  
+  
+  for (i in 1:length(num.fix.dur[,1])){
+    num.fix.dur[i,1] <- max(trimmed.data$FixationIndex[
+      (trimmed.data$ParticipantName == paste(recs[i]))
+      ], na.rm=TRUE)
+  }
+  return(num.fix.dur)
+}
+
+
+## This function extracts the sum fixation duration per AOI
+
+SumFixDurAoi <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName) , aois = unique(trimmed.data$aoi.tag[trimmed.data$aoi.tag != "NA"])){
+  
+  if(missing(trimmed.data)) {stop("You need to enter a data frame")}
+  
+  sum.fix.dur.aoi<-as.data.frame(matrix(nrow=length(recs), ncol=length(aois), NA))
+  names(sum.fix.dur.aoi)<-(aois)
+  row.names(sum.fix.dur.aoi)<-(recs)
+  
+  
+  for (i in 1:length(sum.fix.dur.aoi[,1])) {
+    for(j in 1:length(sum.fix.dur.aoi[1,])) {
+      sum.fix.dur.aoi[i, j] <- sum(trimmed.data$GazeEventDuration[
+        (trimmed.data$ParticipantName == paste(recs[i])) & 
+        (trimmed.data$aoi.tag == paste(aois[j]))], na.rm = TRUE)
+    }
+  }
+  return(sum.fix.dur.aoi)
+}
+
+## This function extracts visit time for AOIs
+
+VisFixDurAoi <- function(AoiData){
+  
+  sum.fix.dur.aoi <- as.data.frame(matrix(nrow=length(AoiData[,1]), ncol=length(AoiData[1,])+1, NA))
+  names(sum.fix.dur.aoi)<-c(names(AoiData), "Times")
+  sum.fix.dur.aoi[1,1:2] <- AoiData[1,1:2]
+  sum.fix.dur.aoi[1,"Stimulus"] <- paste(AoiData[1, "Stimulus"])
+  sum.fix.dur.aoi[1,"Participant"] <- paste(AoiData[1, "Participant"])
+  sum.fix.dur.aoi[1, 5] <- 1
+  k <- 0
+  
+  for (i in 2:length(sum.fix.dur.aoi[,1])) {
+
+    if(AoiData$AOIName[i] == AoiData$AOIName[i-1]) {
+      k <- k + 1;
+      sum.fix.dur.aoi[i-k,"FixationDuration"] = sum.fix.dur.aoi[i-k,"FixationDuration"] + AoiData$FixationDuration[i]
+      sum.fix.dur.aoi[i-k, 5] = sum.fix.dur.aoi[i-k, 5] + 1
+    }
+    else {
+      k <- 0
+      sum.fix.dur.aoi[i,1:2] <- AoiData[i,1:2]
+      sum.fix.dur.aoi[i,"Stimulus"] <- paste(AoiData[i, "Stimulus"])
+      sum.fix.dur.aoi[i,"Participant"] <- paste(AoiData[i, "Participant"])
+      sum.fix.dur.aoi[i, 5] <- 1
+    }
+
+  }
+  #factor(sum.fix.dur.aoi, levels = c(1, 2, 3, 4, 5, 6),
+  #       labels = c("Franz", "Ichen", "James", "Max", "Shenyu", "Yiling"))
+  return(sum.fix.dur.aoi)
+}
+
+
+## This function extracts the length of saccadic run in pixles
+
+LenSac <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName) , aois = unique(trimmed.data$aoi.tag[trimmed.data$aoi.tag != "NA"])) {
+  
+  sac.run<-matrix(nrow=length(trimmed.data[,1]),ncol=1,NA)
+  
+  for (i in 1:length(trimmed.data[,1])){
+    if((is.na(trimmed.data$comb.x.movement[i]) == FALSE)) {
+      sac.run[i] <- ((trimmed.data$comb.x.movement[i])^2 + (trimmed.data$comb.y.movement[i])^2)^0.5
+    } 
+  }
+  
+  len.sac.run<-as.data.frame(matrix(nrow=length(recs), ncol=1, NA))
+  names(len.sac.run)<-"length of saccadic run (px)"
+  row.names(len.sac.run)<-(recs)
+  
+  for (i in 1:length(recs)){
+    len.sac.run[i,]<-mean(sac.run[(trimmed.data$ParticipantName == paste(recs[i]))], na.rm=T)
+  }
+  
+  return(len.sac.run)
+  
+}#end function
+
+
+## This function extracts the length of saccadic run in pixles per AOI
+
+LenSacAOI <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName) , aois = unique(trimmed.data$aoi.tag[trimmed.data$aoi.tag != "NA"])) {
+  
+  sac.run<-matrix(nrow=length(trimmed.data[,1]),ncol=1,NA)
+  
+  for (i in 1:length(trimmed.data[,1])){
+    if((is.na(trimmed.data$comb.x.movement[i]) == FALSE)) {
+      sac.run[i] <- ((trimmed.data$comb.x.movement[i])^2 + (trimmed.data$comb.y.movement[i])^2)^0.5
+    } 
+  }
+  
+  len.sac.run<-as.data.frame(matrix(nrow=length(recs), ncol=length(aois), NA))
+  names(len.sac.run)<-(aois)
+  row.names(len.sac.run)<-(recs)
+  
+  for (i in 1:length(recs)){
+    for (j in 1:length(aois)) {
+      
+      len.sac.run[i, j]<-mean(sac.run[(trimmed.data$ParticipantName == paste(recs[i])) &
+                                      (trimmed.data$aoi.tag == paste(aois[j]))], na.rm=T)
+      
+    }
+    
+  }
+  
+  return(len.sac.run)
+  
+}#end function
+
+
+
+## This function extracts the average saccadic amplitude per AOI
+AvgSacAmpAOI <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName) , aois = unique(trimmed.data$aoi.tag[trimmed.data$aoi.tag != "NA"])){
+  
+  if(missing(trimmed.data)) {stop("You need to enter a data frame")}
+  
+  avg.sac.amp <- as.data.frame(matrix(nrow=length(recs), ncol=length(aois), NA))
+  names(avg.sac.amp)<-(aois)
+  row.names(avg.sac.amp)<-(recs)
+  
+  
+  for (i in 1:length(avg.sac.amp[,1])) {
+    for(j in 1:length(avg.sac.amp[1,])) {
+      avg.sac.amp[i, j] <- mean(trimmed.data$SaccadicAmplitude[
+        (trimmed.data$ParticipantName == paste(recs[i])) & 
+          (trimmed.data$aoi.tag == paste(aois[j]))], na.rm = TRUE)
+    }
+  }
+  return(avg.sac.amp)
+}
+
+## This function extracts the average saccadic amplitude
+AvgSacAmp <- function(trimmed.data , recs = unique(trimmed.data$ParticipantName)){
+  
+  if(missing(trimmed.data)) {stop("You need to enter a data frame")}
+  
+  avg.sac.amp <- as.data.frame(matrix(nrow=length(recs), ncol=1, NA))
+  names(avg.sac.amp)<-"Average Saccadic Amplitude"
+  row.names(avg.sac.amp)<-(recs)
+  
+  
+  for (i in 1:length(avg.sac.amp[,1])) {
+
+    avg.sac.amp[i,] <- mean(trimmed.data$SaccadicAmplitude[
+      (trimmed.data$ParticipantName == paste(recs[i]))], na.rm = TRUE)
+    
+  }
+  return(avg.sac.amp)
 }
--- tobiiDataProcessing.R
+++ tobiiDataProcessing.R
@@ -1,20 +1,45 @@
 rm(list = ls())
 
-setwd("/Users/jing/Desktop/Eyetracking/eyetracking_git/eyeTracking_dp")
+#setwd("/Users/jing/Desktop/Eyetracking/eyetracking_git/eyeTracking_dp")
+setwd("/Users/jingli/Desktop/Eyetracking/R/eyetracking_git")
 
 recordings <- c("sex0")
 numCol <- 9;
 xCol <- 7;
 yCol <- 8;
+aoi.names <- c("color key", "L1", "L2", "L3-leak", "L4", "R1-leak", "R2", "R3", "R4", "S1", "S2-sen_leak", "S3", "S4", "leak")
 
 source("TobiiTrim.R")
 
 
-output_fixation <- TobiiTrim(recs = recordings, fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ", n.aoi = 14)
+trimmed.data <- TobiiTrim(recs = recordings, fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ", n.aoi = 14)
 
-aoi.names <- c("color key", "L1", "L2", "L3-leak", "L4", "R1-leak", "R2", "R3", "R4", "S1", "S2-sen_leak", "S3", "S4", "leak")
-# 
-# recs = recordings
-# fileroot = "./TobiiData/Jing_test01_test01_Rec "
-# dat<-read.table(paste(fileroot, recs[1] , ".tsv", sep=""),sep="\t", header=TRUE)
+Sumfix <- SumFixDur(trimmed.data)
+Numfix <- NumFixDur(trimmed.data)
+Avgfix <- Sumfix/Numfix
 
+SumfixAoi <- SumFixDurAoi(trimmed.data)
+
+
+AoiData <- trimmed.data[(trimmed.data$aoi.tag == "NA") ==F,]
+AoiData <- AoiData[, c("aoi.tag", "GazeEventDuration", "MediaName", "ParticipantName")]
+names(AoiData) <- c("AOIName", "FixationDuration", "Stimulus", "Participant")
+write.csv(AoiData, file = "AoiData.csv", row.names = FALSE, quote = FALSE)
+
+AoiTotFixDur <- aggregate(AoiData$FixationDuration, by=list(AoiData$AOIName), sum)
+names(AoiTotFixDur) <- c("AOIName", "FixationDuration")
+
+AoiVisitTime <- VisFixDurAoi(AoiData)
+AoiVisitTime <- AoiVisitTime[(is.na(AoiVisitTime$AOIName)) == F,]
+write.csv(AoiVisitTime[,1:4], file = "AoiVisitData.csv", row.names = FALSE, quote = FALSE)
+sacrun <- SacRun(trimmed.data)
+
+
+## Avg saccadic amplitude
+avgSacAmp.aoi <- AvgSacAmpAOI(trimmed.data)
+avgSacAmp.all <- AvgSacAmp(trimmed.data)
+
+
+## Avg length of saccades
+avglenSac.all <- LenSac(trimmed.data)
+avglenSac.aoi <- LenSacAOI(trimmed.data)
