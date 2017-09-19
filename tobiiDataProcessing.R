143687cd301d195ce9ec22a945b672c2165ee7d5
Jing <jinli@ucdavis.edu>
2017-09-18 19:02:21 -0700

    somechanges

--- tobiiDataProcessing.R
+++ tobiiDataProcessing.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 
-#setwd("/Users/jing/Desktop/Eyetracking/eyetracking_git/eyeTracking_dp")
-setwd("/Users/jingli/Desktop/Eyetracking/R/eyetracking_git")
+setwd("/Users/jing/Desktop/Eyetracking/eyetracking_git/eyeTracking_dp")
+#setwd("/Users/jingli/Desktop/Eyetracking/R/eyetracking_git")
 
 recordings <- c("sex0")
 numCol <- 9;
@@ -14,17 +14,17 @@ source("TobiiTrim.R")
 
 trimmed.data <- TobiiTrim(recs = recordings, fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ", n.aoi = 14)
 
-Sumfix <- SumFixDur(trimmed.data)
+Sumfix.all <- SumFixDur(trimmed.data)
 Numfix <- NumFixDur(trimmed.data)
-Avgfix <- Sumfix/Numfix
+Avgfix.all <- Sumfix.all/Numfix
 
-SumfixAoi <- SumFixDurAoi(trimmed.data)
+Sumfix.aoi <- SumFixDurAoi(trimmed.data)
 
 
 AoiData <- trimmed.data[(trimmed.data$aoi.tag == "NA") ==F,]
 AoiData <- AoiData[, c("aoi.tag", "GazeEventDuration", "MediaName", "ParticipantName")]
 names(AoiData) <- c("AOIName", "FixationDuration", "Stimulus", "Participant")
-write.csv(AoiData, file = "AoiData.csv", row.names = FALSE, quote = FALSE)
+write.csv(AoiData[1:5,], file = "AoiData.csv", row.names = FALSE, quote = FALSE)
 
 AoiTotFixDur <- aggregate(AoiData$FixationDuration, by=list(AoiData$AOIName), sum)
 names(AoiTotFixDur) <- c("AOIName", "FixationDuration")
@@ -32,7 +32,6 @@ names(AoiTotFixDur) <- c("AOIName", "FixationDuration")
 AoiVisitTime <- VisFixDurAoi(AoiData)
 AoiVisitTime <- AoiVisitTime[(is.na(AoiVisitTime$AOIName)) == F,]
 write.csv(AoiVisitTime[,1:4], file = "AoiVisitData.csv", row.names = FALSE, quote = FALSE)
-sacrun <- SacRun(trimmed.data)
 
 
 ## Avg saccadic amplitude
