bdfc511f5fdbdc5bb7acb672ee1c2a8a38a9d8cc
Jing <jinli@ucdavis.edu>
2017-09-19 19:08:10 -0700

    add group aoi

--- TobiiTrim.R
+++ TobiiTrim.R
@@ -30,14 +30,6 @@ TobiiTrim<-function(recs, fileroot, n.aoi){
     for (j in (numCol+1):(numCol+n.aoi)){dat[is.na(dat[,j]),j]<-0}
     
     
-    # delete missing data
-    #for (j in 1:length(dat[,1])){
-     # if(is.na(dat[j, 1]) == TRUE)
-     # {
-      #  print(j)
-    #  }
-   # }
-    
     ### This is the main bit
     
     # this bit extracts the saccades/regressions
@@ -110,6 +102,92 @@ TobiiTrim<-function(recs, fileroot, n.aoi){
   
   return(dat)
 }
+
+## This function turns data into a more manageable format
+TobiiTrim.noAOIs<-function(recs, fileroot){
+  
+  # this sets up the first line (which will be deleted) of a matrix to store all the data.  This is done so we can rbind().
+  
+  first.line<-matrix(nrow=1, ncol=(numCol+2), NA)
+  
+  #This part checks that the recodrings that you say are there are actually there taking time now but saving pain later on.  Fix this to just real 1 line.
+  
+  #for (j in 1:length(recs)){dat<-read.table(paste(fileroot, recs[j] , ".tsv", sep=""),sep="\t", header=TRUE) ; print(paste("Checking file", recs[j]))}
+  
+  # this is a 'loop' that cycles through each for the recs in turn
+  for (i in 1:length(recs)){
+    
+    # progress bar
+    
+    print(paste("Analysing Recording", recs[i]))
+    
+    # this loads the data
+    dat<-read.table(paste(fileroot, recs[i] , ".tsv", sep=""),sep="\t", header=TRUE)
+    dat=dat[,1:numCol]
+    
+    
+    # this reduces the size of the data because it is too big and that slows things down later on
+    dat <- unique(dat)
+    dat<-dat[dat$GazeEventType == "Fixation",]
+    #dat<-dat[dat$GazeEventType != "Unclassified",]
+    
+    
+    ### This is the main bit
+    
+    # this bit extracts the saccades/regressions
+    # here we create the additional variables to hold the saccades/regressions
+    
+    
+    ## This extracts all x axis movement       
+    
+    comb.x.movement<-matrix(ncol=1, nrow=length(dat[,1]), NA)
+    
+    for (k in 2:(length(dat[,1])))    {comb.x.movement[k]<-(dat[k,xCol]-dat[k-1,xCol])}                               
+    
+    ## This loop extracts the Y-axis movement.  We need this for checking whether a saccade is a reading saccade or not.
+    
+    comb.y.movement<-matrix(ncol=1, nrow=length(dat[,1]), NA)
+    
+    for (k in 2:(length(dat[,1])))    {comb.y.movement[k]<-dat[k,yCol]-dat[k-1,yCol]}       
+    
+    ## pop all the data togheter for later                        }
+    
+    dat<-cbind(dat, comb.x.movement ,comb.y.movement)
+    
+    ## ok we have the measures
+    ## add a name tag to the person
+    #tag<-matrix(ncol=1, nrow=length(dat[,1]), paste("Rec",recs[i]))
+    #dat2<-cbind(tag, dat)
+    ## put all the data together in 1 dataframe
+    
+    first.line<-as.data.frame(first.line)
+    names(first.line)<-names(dat)
+    if (i==1) {dat3<-rbind(dat, first.line)}
+    if (i>1) {dat3<-rbind(dat3, dat, first.line)}
+  }
+  
+  #########  This is where the main loop ends, below is tidying up
+  
+  dat<-dat3
+  dat<-dat[is.na(dat$ParticipantName)==F,]
+  
+  return(dat)
+}
+
+
+## This function turns data into a more manageable grouped format
+GroupAois <- function(trimmed.data) {
+  
+  dat <- trimmed.data
+  dat$AOIName[grep(".leak", dat$AOIName)] <- "leakNodes"
+  dat$AOIName[grep(".key", dat$AOIName)] <- "colorKey"
+  dat$AOIName[grep("\\.", dat$AOIName)] <- "otherNodes"
+  dat$AOIName[grep("NA", dat$AOIName)] <- "others"
+  
+  return(dat)
+}
+
+
 
 ## This function extracts the sum fixation duration
 
--- tobiiDataProcessing.R
+++ tobiiDataProcessing.R
@@ -1,38 +1,29 @@
 rm(list = ls())
-
 setwd("/Users/jing/Desktop/Eyetracking/eyetracking_git/eyeTracking_dp")
 #setwd("/Users/jingli/Desktop/Eyetracking/R/eyetracking_git")
 
-recordings <- c("sex0")
+recordings <- c("sex0", "mit0")
+num.aois <- c(14, 16)
 numCol <- 9;
 xCol <- 7;
 yCol <- 8;
-aoi.names <- c("color key", "L1", "L2", "L3-leak", "L4", "R1-leak", "R2", "R3", "R4", "S1", "S2-sen_leak", "S3", "S4", "leak")
 
 source("TobiiTrim.R")
 
+rm(trimmed.data)
+trimmed.data.list <- lapply(1:length(recordings), function(x) TobiiTrim(recs = recordings[x], 
+                               fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ", n.aoi = num.aois[x]))
+names(trimmed.data.list) <- recordings
 
-trimmed.data <- TobiiTrim(recs = recordings, fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ", n.aoi = 14)
+trimmed.data.noAoi <- TobiiTrim.noAOIs(recs = recordings, fileroot = "./Jing_EyetrackingData/Perceptual_Masking_test_test1_Rec ")
 
+## Total fixation duration
 Sumfix.all <- SumFixDur(trimmed.data)
 Numfix <- NumFixDur(trimmed.data)
 Avgfix.all <- Sumfix.all/Numfix
 
+## Total fixation duration per aoi
 Sumfix.aoi <- SumFixDurAoi(trimmed.data)
-
-
-AoiData <- trimmed.data[(trimmed.data$aoi.tag == "NA") ==F,]
-AoiData <- AoiData[, c("aoi.tag", "GazeEventDuration", "MediaName", "ParticipantName")]
-names(AoiData) <- c("AOIName", "FixationDuration", "Stimulus", "Participant")
-write.csv(AoiData[1:5,], file = "AoiData.csv", row.names = FALSE, quote = FALSE)
-
-AoiTotFixDur <- aggregate(AoiData$FixationDuration, by=list(AoiData$AOIName), sum)
-names(AoiTotFixDur) <- c("AOIName", "FixationDuration")
-
-AoiVisitTime <- VisFixDurAoi(AoiData)
-AoiVisitTime <- AoiVisitTime[(is.na(AoiVisitTime$AOIName)) == F,]
-write.csv(AoiVisitTime[,1:4], file = "AoiVisitData.csv", row.names = FALSE, quote = FALSE)
-
 
 ## Avg saccadic amplitude
 avgSacAmp.aoi <- AvgSacAmpAOI(trimmed.data)
@@ -42,3 +33,26 @@ avgSacAmp.all <- AvgSacAmp(trimmed.data)
 ## Avg length of saccades
 avglenSac.all <- LenSac(trimmed.data)
 avglenSac.aoi <- LenSacAOI(trimmed.data)
+
+
+#AoiData <- trimmed.data[(trimmed.data$aoi.tag == "NA") ==F,]
+AoiData <- trimmed.data_sex0[, c("aoi.tag", "GazeEventDuration", "MediaName", "ParticipantName")]
+names(AoiData) <- c("AOIName", "FixationDuration", "Stimulus", "Participant")
+write.csv(AoiData, file = "AoiData.csv", row.names = FALSE, quote = FALSE)
+
+
+## Fixation duration per aoi
+AoiTotFixDur <- aggregate(AoiData$FixationDuration, by=list(AoiData$AOIName), sum)
+names(AoiTotFixDur) <- c("AOIName", "FixationDuration")
+
+
+
+AoiVisitTime <- VisFixDurAoi(AoiData)
+AoiVisitTime <- AoiVisitTime[(is.na(AoiVisitTime$AOIName)) == F,]
+write.csv(AoiVisitTime[,1:4], file = "AoiVisitData.csv", row.names = FALSE, quote = FALSE)
+
+AoiVisitTime.group <- GroupAois(AoiVisitTime)
+write.csv(AoiVisitTime.group[,1:4], file = "AoiVisitData_group.csv", row.names = FALSE, quote = FALSE)
+
+
+
